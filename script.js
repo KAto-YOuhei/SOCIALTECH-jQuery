1bf8ff71757a95f60c71bc981010ac62e45a2dd0
KAto-YOuhei <you42691@gmail.com>
2023-04-25 22:19:38 +0900

    カルーセル画像にリンク先を添付反映

--- script.js
+++ script.js
@@ -19,4 +19,13 @@ $(function () {
       100
     );
   });
+
+  //カルーセル
+  $(".carousel").slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  });
 });
